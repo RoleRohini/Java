dfd62b10fdf18a9fe966fbcc60021dd3dd2f0193
Role Rohini <123159032+RoleRohini@users.noreply.github.com>
2026-01-31 15:35:29 +0530

    Add files via upload
    Signed-off-by: Role Rohini <123159032+RoleRohini@users.noreply.github.com>

---
+++ A.java
@@ -0,0 +1,17 @@
+public class A {
+short y=180;
+static byte x=100;
+
+public static void main(String [] arg){
+A n =new A();
+n.get();
+
+}
+
+public void get(){
+System.out.println(y);
+System.out.println(x);
+}
+
+
+}
