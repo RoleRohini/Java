368b19f130cf46f09668401172f2d8781ce5444d
Role Rohini <123159032+RoleRohini@users.noreply.github.com>
2026-03-13 16:51:29 +0530

    Add files via upload
    Signed-off-by: Role Rohini <123159032+RoleRohini@users.noreply.github.com>

---
+++ arrtwo1.java
@@ -0,0 +1,19 @@
+public class arrtwo1
+{
+public static void main(String[] arg)
+{
+int [][] a= {{1,2,3,4},{6,7,8}};
+for(int i=0;i<a.length;i++)
+{
+for(int j=0;j<a[i].length;j++)
+{
+System.out.print(a[i][j]+ " ");
+}
+System.out.println( );
+}
+
+}
+}
+// output
+// 1 2 3 4
+// 6 7 8
